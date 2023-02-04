ddf1fc205d42567562bc16464d472b245844c374
javokhirsobirov <dilya.husenova11.78@gmail.com>
2023-02-04 22:26:34 +0500

    first commit

---
+++ java.js
@@ -0,0 +1,14 @@
+let arr = [1, 2, false, 'hello', 24, 'world', undefined, null, 'error', 22, {name: 'car'}]
+
+let str = []
+let num = []
+let bool = []
+let obj = []
+
+arr.filter(item => {
+    if(arr.forEach(element => {
+        
+    });) {
+       num.push(item) 
+    } else if()
+})
